f42cb858943acc5c8329dc0b6b11004abf0d5c8d
KnockingmasterJiro <arinamuraskina34@gmail.com>
2025-04-16 18:43:00 +0900

    Update layout.tsx

--- japan-facility-database/app/layout.tsx
+++ japan-facility-database/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { GeistSans, GeistMono } from "@vercel/fonts";
 import "./globals.css";
 
-const geistSans = Geist({
+// フォントの初期化
+const geistSans = GeistSans({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = GeistMono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
